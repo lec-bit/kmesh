ad186e1b4659594b54fe1d817a8c42af10100871
lec-bit <glfhmzmy@126.com>
2024-11-05 05:18:42 +0800

    fix gen-copyright
    Signed-off-by: lec-bit <glfhmzmy@126.com>

--- hack/gen-check.sh
+++ hack/gen-check.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-function ignore(){
-  git update-index --assume-unchanged bpf/include/bpf_helper_defs_ext.h 
-  git update-index --assume-unchanged config/kmesh_marcos_def.h 
-  git update-index --assume-unchanged bpf/kmesh/bpf2go/bpf2go.go
-  git update-index --assume-unchanged mk/api-v2-c.pc 
-  git update-index --assume-unchanged mk/bpf.pc
-}
-
-ignore
-
 if [[ -n $(git status --porcelain) ]]; then
   git status
   git diff
